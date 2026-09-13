78eef5c39f93a197a6045d3731c3b586b4fb2da3
Saket Bekal <saket.98b@gmail.com>
2026-09-13 11:57:13 +0530

    nat-gw added

--- 02-core-networking/main.tf
+++ 02-core-networking/main.tf
@@ -43,6 +43,8 @@ resource "azurerm_network_security_group" "this" {
       direction                  = "Inbound"
     }
   }
+
+  tags = var.tags
 }
 
 resource "azurerm_subnet_network_security_group_association" "this" {
@@ -62,6 +64,7 @@ resource "azurerm_route_table" "this" {
     address_prefix = "0.0.0.0/0"
     next_hop_type  = "Internet"
   }
+  tags = var.tags
 }
 
 resource "azurerm_subnet_route_table_association" "this" {
@@ -70,5 +73,38 @@ resource "azurerm_subnet_route_table_association" "this" {
   route_table_id = azurerm_route_table.this.id
 }
 
+resource "azurerm_public_ip" "public" {
+  count               = var.enable_nat_gateway ? 1 : 0
+  name                = "natgw-public-ip"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
 
-# TODO: Optionally create public IP + NAT Gateway + subnet associations when enable_nat_gateway is true.
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_nat_gateway" "this" {
+  count               = var.enable_nat_gateway ? 1 : 0
+  name                = "nat-gw"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  tags = var.tags
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "this" {
+  count                = var.enable_nat_gateway ? 1 : 0
+  nat_gateway_id       = azurerm_nat_gateway.this[0].id
+  public_ip_address_id = azurerm_public_ip.public[0].id
+}
+
+resource "azurerm_subnet_nat_gateway_association" "this" {
+  for_each       = var.enable_nat_gateway ? var.subnets : {}
+  subnet_id      = azurerm_subnet.this[each.key].id
+  nat_gateway_id = azurerm_nat_gateway.this[0].id
+}
